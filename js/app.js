914aa823a44b68f61c9e6e96dd91c4d82919606d
melissayauri <yaurimelissa@gmail.com>
2017-11-14 10:19:26 -0500

    incorporando la creacion de nuevos tweets

--- js/app.js
+++ js/app.js
@@ -10,16 +10,39 @@ function enterText(){
   var showText = document.getElementById('caja');
   /*obtine el texto ingresado*/
   var myText =   document.getElementById('entrada').value ;
+
+  var newText = document.createElement("div");
+
+  newText.innerHTML = myText;
+  showText.appendChild(newText);
   /*añade ele texto en la caja de visualizacion*/
-   showText.innerHTML = myText
+  /* showText.innerHTML = myText*/
+
 
    if(myText == 0 || myText == null){
      alert("el campo vacio");
    }
 }
 
-function validar(tweet){
-var tweet = document.formul.text;
-document.getElementById("contador").innerHTML = document.getElementById('entrada').value.length;
+function validar(){
+/*var tweet = document.formul.text;*/
+/*document.getElementById("contador").innerHTML = document.getElementById('entrada').value.length;*/
+var contador= document.getElementById("contador");
+var boton = document.getElementById('enviar');
+boton.disabled= false;
+var limite = 140;
+var tamaño = document.getElementById('entrada').value.length ;
+var contando = document.getElementById('contador').innerHTML = limite-tamaño;
+if(tamaño>=140){
+  contador.style.color="red";
+  boton.disabled= true;
+}
+else if(tamaño>=120){
+  contador.style.color="blue";
+  boton.disabled= false;
+}
+else{
+  contador.style.color="black";
+}
 }
   /*document.formul.button.onclick = enterText;*/
