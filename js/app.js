9b390907142ef889face8323ea1040aa223f08f5
melissayauri <yaurimelissa@gmail.com>
2017-11-16 14:30:11 -0500

    incorporando funcionalidad para los nuevos tweets

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
+window.addEventListener('load', function(event){
+  /*boton enviar del formulario*/
+  var buttonSend = document.getElementById('enviar');
+  /*variable cuando se ingresa el texto*/
+  var text =   document.getElementById('entrada')
+  /*variable del contendor de los nuevos textos*/
+  var showText = document.getElementById('caja');
+  /*llamando al evento click*/
+  buttonSend.addEventListener('click',enterText);
+  /*funcion para ingresar el texto*/
+  function enterText(){
+    /*valor de la entrada del texto*/
+    var myText = text.value;
+    /*aplica para que la entrada del texto se vacie luego del click*/
+    text.value = "";
+    /*variable de los nuevos textos*/
+    var newText = document.createElement("div");
+    /*incoporando el texto ingresado en los nuevos texto que se crea debajo*/
+    newText.textContent = myText;
+    /*creando los nuevos textos*/
+    showText.appendChild(newText);
+  }
+
+
+
+})
+
+
+/*
 window.onload = function(){
 document.getElementById('enviar').addEventListener('click',enterText);
  document.getElementById('entrada').addEventListener("keydown",validar);
@@ -6,9 +35,9 @@ document.getElementById('enviar').addEventListener('click',enterText);
 
 function enterText(){
 
-  /*variable en donde se visualizara*/
+  /*variable en donde se visualizara*
   var showText = document.getElementById('caja');
-  /*obtine el texto ingresado*/
+  /*obtine el texto ingresado
   var myText =   document.getElementById('entrada').value ;
 
   var newText = document.createElement("div");
@@ -16,7 +45,7 @@ function enterText(){
   newText.innerHTML = myText;
   showText.appendChild(newText);
   /*añade ele texto en la caja de visualizacion*/
-  /* showText.innerHTML = myText*/
+  /* showText.innerHTML = myText*
 
 
    if(myText == 0 || myText == null){
@@ -27,7 +56,7 @@ function enterText(){
 function validar(){
   /*incorporando la validacion*/
 /*var tweet = document.formul.text;*/
-/*document.getElementById("contador").innerHTML = document.getElementById('entrada').value.length;*/
+/*document.getElementById("contador").innerHTML = document.getElementById('entrada').value.length;*
 var contador= document.getElementById("contador");
 var boton = document.getElementById('enviar');
 boton.disabled= false;
