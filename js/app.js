2d8a814dbc57e7d1a833b8ea5667d11d1691fc84
melissayauri <yaurimelissa@gmail.com>
2017-11-18 02:16:12 -0500

    Incorporando la guia de estilos

--- js/app.js
+++ js/app.js
@@ -1,69 +1,78 @@
-window.addEventListener('load', function(event){
-  /*boton enviar del formulario*/
+/* llamando a la ventana para que cargen todas la funciones*/
+window.addEventListener('load', function(event) {
+  /* boton tweet del formulario*/
   var buttonTweet = document.getElementById('send');
-  /*variable cuando se ingresa el texto*/
-  var text =   document.getElementById('input-text')
-  /*variable del contendor de los nuevos textos*/
+  /* variable para el textarea para ingresar el texto*/
+  var text = document.getElementById('input-text');
+  /* variable del contendor de los nuevos textos*/
   var showText = document.getElementById('box-newtext');
-  /*contador*/
+  /* contador*/
   var count = document.getElementById('count');
-  /*llamando al evento click*/
-  buttonTweet.addEventListener('click',enterText);
-  /*funcion para ingresar el texto*/
-  function enterText(){
-    /*valor de la entrada del texto*/
-    var myText = text.value;
-    /*aplica para que la entrada del texto se vacie luego del click*/
-    text.value = "";
-    if(myText == 0 || myText == null){
+  /* llamando al evento click* en el boton tweet*/
+  buttonTweet.addEventListener('click', enterText);
+  /* funcion para ingresar el texto*/
+  function enterText() {
+    /* valor de la entrada del texto más la hora*/
+    var myText = text.value + '<br>' + moment().format('LT');
+    /* aplica para que la entrada del texto se vacie luego del click*/
+    text.value = '' ;
+    /* validando si se ingresa un campo vacío*/
+    if (myText === 0 || myText === null) {
+      /* desabilitando el botón tweet*/
       buttonTweet.disabled = true;
+    } else {
+      /* variable de los nuevos textos*/
+      var newText = document.createElement('div');
+      /* incoporando el texto ingresado en el elemento div*/
+      newText.innerHTML = myText ;
+      /* creando los nuevos textos*/
+      showText.appendChild(newText);
     }
-    else{
-    /*variable de los nuevos textos*/
-    var newText = document.createElement("div");
-    /*incoporando el texto ingresado en los nuevos texto que se crea debajo*/
-    newText.textContent = myText;
-    /*creando los nuevos textos*/
-    showText.appendChild(newText);}
-
   }
-/*llamando a la tecla*/
+  /* ejecutando la tecla pulsada en la entrada del texto*/
   text.addEventListener('keydown', countCharacters);
-  /**/
-    function countCharacters(){
-      buttonTweet.disabled=false;
-      var limitedCharacters= 140;
-      var size = text.value.length;
-      count.innerHTML= limitedCharacters-size;
-
-      if(size>=140){
-        /*count.style.color="green";*/
-        count.setAttribute("id","red");
-        buttonTweet.disabled= true;
-      }
-      else if(size>=120 && size<=129){
-        count.setAttribute("id","purple");
-        buttonTweet.disabled= false;
-      }
-      else if(size>=130 && size<=139){
-        count.setAttribute("id","blue");
-        buttonTweet.disabled= false;
-      }
-      else{
-        count.setAttribute("id","count");
-      }
+  /* función de contador de caracteres*/
+  function countCharacters() {
+    /* limite de caracteres permitidos, 140 palabras*/
+    var limitedCharacters = 140;
+    /* tamaño del texto ingresado*/
+    var size = text.value.length;
+    /* añadiendo al contador el conteo de manera descendente*/
+    count.innerHTML = (limitedCharacters - size) - 1;
+    /* validando si el texto ingresado es mayor a 140 palabras,valor negativo*/
+    if (size >= 140) {
+      /* incorporando el id que contiene el color rojo para el contador*/
+      count.setAttribute('id', 'red');
+      /* el botón se desabilita*/
+      buttonTweet.disabled = true;
+      /* validando si el texto ingresado tiene entre 120 y 129 caracteres */
+    } else if (size >= 120 && size <= 129) {
+      /* incorpora el color morado al contador */
+      count.setAttribute('id', 'purple');
+      /* el botón se habilita*/
+      buttonTweet.disabled = false;
+      /* validando si el texto ingresado tiene entre 130 y 139 caracteres */
+    } else if (size >= 130 && size <= 139) {
+      /* incorpora el color azul al contador */
+      count.setAttribute('id', 'blue');
+      /* el botón se habilita*/
+      buttonTweet.disabled = false;
+    } else {
+      /* incorpora el color inicial al contador */
+      count.setAttribute('id', 'count');
     }
-text.addEventListener('keydown',enter);
-function enter(){
-
-text.setAttribute("id","big");
-
-}
-
-buttonTweet .addEventListener('click',en);
-function en(){
-
-text.setAttribute("id","input-text");
-
-}
-  })
+  }
+  /* llamando al pulsar la tecla*/
+  text.addEventListener('keydown', adjustContent);
+  /* funcion para que la entrada se adecue al texto*/
+  function adjustContent() {
+    /* se aplica al texarea para que se adecue a la altura del contenido*/
+    text.style.cssText = 'height:' + text.scrollHeight + 'px';
+  }
+  /* llamando al evento click en el boton tweet*/
+  buttonTweet .addEventListener('click', normalContent);
+  /* funcion para que el texarea vuelva al estado normal despues de enviar un texto*/
+  function normalContent() {
+    text.style.cssText = 'height' + 'px';
+  }
+});
